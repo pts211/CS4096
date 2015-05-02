0d0046780e5382d82fab77dea7803c037a080f8f
Paul Sites <pts211@gmail.com>
2015-05-02 17:03:41 -0500

    Removed trackbars

--- OpenCV/camera.hpp
+++ OpenCV/camera.hpp
@@ -9,16 +9,13 @@ void camera::update()
 	floorsign = ""; //NO IMPLEMENTATION YET
 
 	
-	VideoCapture cap(0); // open the video camera no. 0
+	
 	while(!cap.isOpened())  // if not success, exit program
     {
         cout << "error cannot read from the camera" << endl;
     }
 	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
 	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-	cout << "Frame size : " << dWidth << " x " << dHeight << endl;//640x480
-	
-
 
 	int x_length = 200;
 	int y_length = 200;
@@ -359,13 +356,14 @@ void camera::update()
 
 void camera::output()
 {
-  	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    
+  	namedWindow("MyVideo",CV_WINDOW_OPENGL); //create a window called "MyVideo"
   	imshow("MyVideo", input); //show the frame in "MyVideo" window
-  	if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+  	/*if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
   	{
   		cout << "esc key is pressed by user" << endl;
       exit(1);
-  	}
+  	}*/
   
   	cout << "[ " << pathisblocked << ", " << floorsign << ", " << e_slope << ", " << c_slope << " ]" << endl;
   
